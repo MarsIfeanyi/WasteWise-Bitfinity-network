0e338d8fe46a4ec2f3b93f9ef9cfd49482bb754a
Koniesjay <jkonyeri@gmail.com>
2024-03-26 22:24:13 +0100

    first commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,47 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "tailwindcss";
+import { fileURLToPath, URL } from 'url';
+import environment from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      process: 'process/browser',
-      util: 'util',
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
     },
   },
-  plugins: [react()],
+  build: {
+    emptyOutDir: true,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:4943",
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [
+    react(),
+    environment("all", { prefix: "CANISTER_" }),
+    environment("all", { prefix: "DFX_" }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: "declarations",
+        replacement: fileURLToPath(
+          new URL("../declarations", import.meta.url)
+        ),
+      },
+    ],
+  },
 })
